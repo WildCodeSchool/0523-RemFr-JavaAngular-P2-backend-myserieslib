d27435904df159bf915c31fc4e203eb8f3c7d680
TRINH <laser2u02@gmail.com>
2023-07-26 17:00:28 +0200

    category route

--- src/main/java/com/templateproject/api/controller/CategorieController.java
+++ src/main/java/com/templateproject/api/controller/CategorieController.java
@@ -1,16 +1,17 @@
 package com.templateproject.api.controller;
 
-import com.github.javafaker.Cat;
 import com.templateproject.api.dto.CategoryDto;
 import com.templateproject.api.entity.Category;
 import com.templateproject.api.repository.CategoryRepository;
 import com.templateproject.api.service.DtoConversionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -31,7 +32,8 @@ public class CategorieController {
     }
 
     @PostMapping("")
-    public Category createCategory(@RequestParam String categoryName) {
+    public Category createCategory(@RequestBody Map<String, String> requestBody) {
+        String categoryName = requestBody.get("categoryName");
         Category existingCategory = categoryRepository.findByName(categoryName);
         if (existingCategory != null) {
             return existingCategory;
@@ -40,14 +42,22 @@ public class CategorieController {
         return categoryRepository.save(newCategory);
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteCategoryById(@PathVariable UUID id){
+        categoryRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategoryById(@PathVariable UUID id, @RequestBody Category newCategory){
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Category not found"));
+        category.setName(newCategory.getName());
+        return categoryRepository.save(category);
+    }
+
     @GetMapping("/categoriesWithSeries")
     public List<CategoryDto> getAllCategoriesWithSeries() {
         List<Category> categories = categoryRepository.findAll();
         return categories.stream().map(dtoConversionService::convertToCategoryDto).collect(Collectors.toList());
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable UUID id) {
-        categoryRepository.deleteById(id);
-    }
 }
